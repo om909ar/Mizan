a68b096915c5538c363898759c9cbe636ef63ebb
om909ar <139689383+om909ar@users.noreply.github.com>
2026-07-14 14:29:36 +0300

    Update details.js

--- details.js
+++ details.js
@@ -1,7 +1,21 @@
+const history = JSON.parse(localStorage.getItem("history")) || [];
+
+const index = localStorage.getItem("selectedHistory");
+
+const item = history[index];
+
+const monthTitle = document.getElementById("monthTitle");
+
 const detailsBox = document.getElementById("detailsBox");
 
+monthTitle.textContent = item.month;
+
 detailsBox.innerHTML = `
-<div class="money-box">
-<h2>سيتم عرض تفاصيل الشهر هنا</h2>
-</div>
+    <div class="money-box">
+
+        <h2>مبلغ التصدق</h2>
+
+        <h1>${item.balance} ريال</h1>
+
+    </div>
 `;
