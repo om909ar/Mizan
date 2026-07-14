34f033e69edf38a84f1f49b681e5f9286a795371
om909ar <139689383+om909ar@users.noreply.github.com>
2026-07-14 13:59:05 +0300

    Update history.js

--- history.js
+++ history.js
@@ -1,3 +1,34 @@
 const history = JSON.parse(localStorage.getItem("history")) || [];
 
-console.log(history);
+const historyList = document.getElementById("historyList");
+
+if (history.length === 0) {
+
+    historyList.innerHTML = `
+        <div class="money-box">
+            <h2>لا توجد سجلات حتى الآن</h2>
+            <p style="color:#9CA3AF;">
+                سيتم حفظ كل شهر هنا بعد الضغط على
+                <br>
+                <strong>تم التصدق</strong>
+            </p>
+        </div>
+    `;
+
+} else {
+
+    history.forEach(item => {
+
+        historyList.innerHTML += `
+            <div class="money-box">
+
+                <h2>${item.month}</h2>
+
+                <h1>${item.balance} ريال</h1>
+
+            </div>
+        `;
+
+    });
+
+}
