86533a312fe2464f2bfdfd3a627c7a67bb35fc61
om909ar <139689383+om909ar@users.noreply.github.com>
2026-07-14 17:06:02 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,21 @@
 const settingsBox = document.getElementById("settingsBox");
 
 settingsBox.innerHTML = `
-    <div class="money-box">
 
-        <h2>الإعدادات قريبًا ⚙️</h2>
+<div class="money-box">
+
+    <h2>💰 قيمة الصلاة</h2>
+
+    <div class="setting-row">
+
+        <button id="prayerMinus">➖</button>
+
+        <span id="prayerValue">5 ريال</span>
+
+        <button id="prayerPlus">➕</button>
 
     </div>
+
+</div>
+
 `;
