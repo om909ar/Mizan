b0b12a90245bed7974e430cd94eaa1572bc28a14
om909ar <139689383+om909ar@users.noreply.github.com>
2026-07-14 14:46:27 +0300

    Update details.js

--- details.js
+++ details.js
@@ -13,9 +13,28 @@ monthTitle.textContent = item.month;
 detailsBox.innerHTML = `
     <div class="money-box">
 
-        <h2>مبلغ التصدق</h2>
-
+        <h2>💰 مبلغ التصدق</h2>
         <h1>${item.balance} ريال</h1>
+
+        <hr>
+
+        <p>🕌 الفجر : ${item.stats.fajr}</p>
+        <p>🕌 الظهر : ${item.stats.dhuhr}</p>
+        <p>🕌 العصر : ${item.stats.asr}</p>
+        <p>🕌 المغرب : ${item.stats.maghrib}</p>
+        <p>🕌 العشاء : ${item.stats.isha}</p>
+
+        <hr>
+
+        <p>❌ الذنب : ${item.stats.sin}</p>
+
+        <hr>
+
+        <p>🤲 الوتر : ${item.stats.witr}</p>
+        <p>☀️ الضحى : ${item.stats.duha}</p>
+        <p>📿 الذكر : ${item.stats.zikr}</p>
+        <p>📖 القرآن : ${item.stats.quran}</p>
+        <p>❤️ عمل الخير : ${item.stats.good}</p>
 
     </div>
 `;
